d8ceb32f963d8eee2aaafacfc6633fedb6859710
Diego Pamo <diego.pamo@tecsup.edu.pe>
2021-04-14 18:09:28 -0500

    Punto 6 perteneciente a Parte 2

--- Parte2/parte2/src/App.js
+++ Parte2/parte2/src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',number: 973696215 }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const eventRename = (event) => {
     setNewName(event.target.value)
   }
+  const eventNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
 
   const eventClick = (event) => {
 
     event.preventDefault()
-    const person = {name: newName}
+    const person = {name: newName, number:newNumber}
 
     var cont=1 
 
@@ -27,9 +32,12 @@ const App = () => {
       setPersons([...persons, person])
       window.alert('El nombre '+NewName+' fue agregado correctamente');
       setNewName("")
+      setNewNumber("")
     } else {
       window.alert('El nombre ' +NewName+' ya existe en su lista');
       setNewName("")
+      setNewNumber("")
+
     }
 
   }
@@ -38,16 +46,22 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={eventClick}>
         <div>
           name: <input type="text" value={newName} onChange={eventRename} />
+        </div>
+        <div>
+          number: <input type="number" value={newNumber} onChange={eventNewNumber} />
         </div>
         <div>
           <button type="submit"  onClick={eventClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-          {persons.map((person,index) => {return(<li key={index}>{person.name}</li>)})}
+          {persons.map((person,index) => {
+            return(
+              <li key={index}>{person.name} whit number is {person.number}</li>
+              )})}
     </div>
   )
 }
